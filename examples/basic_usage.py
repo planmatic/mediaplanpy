b0ef64f7ba3a94c48646c1c3bf264246f3b4dc8f
Laurent Colard <laurent.colard@level5i.com>
2025-04-30 17:56:52 -0400

    EOD Push - now working on a separate project after pip install including both script and CLI

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,8 +1,9 @@
 from mediaplanpy import WorkspaceManager
 
 # Load a workspace
-workspace = WorkspaceManager()
+workspace = WorkspaceManager(r"C:\Users\laure\PycharmProjects\mediaplanpy\examples\fixtures\sample_workspace.json")
 workspace.load()
 
 # Get storage configuration
 storage_config = workspace.get_storage_config()
+print(storage_config)
